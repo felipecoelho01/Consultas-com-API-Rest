d24bbfd4a1f54aa3d9a21737eac247ccb81a0b93
felipecoelho01 <felipecoelho12@outlook.com.br>
2025-04-22 23:21:29 -0300

    first commit[

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+
+  return (
+    <>
+      <div className='container bg-red-400 mx-auto'>
+        <h1> teste</h1>
+      </div>
+ 
+    </>
+  )
+}
+
+export default App
